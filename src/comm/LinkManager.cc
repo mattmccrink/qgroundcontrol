83f04dbdb028c47b9bc04cfe909c2c99c3c7a599
Matt McCrink <mattmccrink@gmail.com>
2016-11-11 11:26:16 -0500

    master

--- src/comm/LinkManager.cc
+++ src/comm/LinkManager.cc
@@ -193,7 +193,7 @@ void LinkManager::_addLink(LinkInterface* link)
                 link->_setMavlinkChannel(i);
                 // Start the channel on Mav 1 protocol
                 mavlink_status_t* mavlinkStatus = mavlink_get_channel_status(i);
-                mavlinkStatus->flags = mavlink_get_channel_status(i)->flags | MAVLINK_STATUS_FLAG_OUT_MAVLINK1;
+                mavlinkStatus->flags = MAVLINK_STATUS_FLAG_OUT_MAVLINK1; //mavlink_get_channel_status(i)->flags | MAVLINK_STATUS_FLAG_OUT_MAVLINK1;
                 qDebug() << "LinkManager mavlinkStatus" << mavlinkStatus << i << mavlinkStatus->flags;
                 _mavlinkChannelsUsedBitMask |= 1 << i;
                 channelSet = true;
